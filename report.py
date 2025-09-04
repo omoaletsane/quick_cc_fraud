8a4a91577a756f85f4e6449b3362f03b76aa564d
Oduetse Moaletsane <omoaletsane@gmail.com>
2025-09-04 14:08:00 +0200

    chore: save local changes before rebase

--- report.py
+++
@@ -1,6 +0,0 @@
-﻿from reportlab.lib.pagesizes import letter
-from reportlab.pdfgen import canvas
-
-c = canvas.Canvas("output.pdf", pagesize=letter)
-c.drawString(100, 750, "quick_cc_fraud")
-c.save()
